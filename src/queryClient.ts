2184bfc715bd8507960f548fc5d24428dff0b254
jinah-byun <wlsdk120@gmail.com>
2023-03-04 23:26:44 +0900

    feat: react-query 연결

---
+++ src/queryClient.ts
@@ -0,0 +1,17 @@
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
+import { getTodos, postTodo } from "../my-api";
+
+// Create a client
+export const getClient = (() => {
+  let client: QueryClient | null = null;
+  return () => {
+    if (!client) client = new QueryClient({});
+    return client;
+  };
+})();
