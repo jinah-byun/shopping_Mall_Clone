dd14d9f3d3357b71fbfbc5799a613be5a297c43a
jinah-byun <wlsdk120@gmail.com>
2023-03-24 09:39:24 +0900

    feat:상품목록가져오기

--- src/pages/products/idIndex.tsx
+++ src/pages/products/idIndex.tsx
@@ -1,3 +1,14 @@
-const ProductList = () => <div>상품목록</div>;
+import { useQuery } from "react-query";
+import { fetcher, QueryKeys } from "../../queryClient";
 
+const ProductList = () => {
+  const { data } = useQuery(QueryKeys.PRODUCTS, () =>
+    fetcher({
+      method: "GET",
+      path: "/products",
+    })
+  );
+  console.log(data);
+  return <div>상품목록</div>;
+};
 export default ProductList;
